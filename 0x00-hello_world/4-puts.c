0eb6eadfcfd63c1fdaccd6df75be336b814ca840
JamalGuyo <jamalguyo08@gmail.com>
2022-06-30 21:52:31 +0300

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+
 /*
  *
  * using put to print to std output
  */
 int main(void)
 {
-    char msg[] = "\"Programming is like building a multilingual puzzle";
-    puts(msg);
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
