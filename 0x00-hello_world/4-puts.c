e99b3f28700b44643fd0d7a42ebfa625e394c697
JamalGuyo <jamalguyo08@gmail.com>
2022-06-30 22:19:24 +0300

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/*
- *
- * using put to print to std output
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
