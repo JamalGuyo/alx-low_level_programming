ce6b1aade949c6001611d5a540eea543cbf7d990
JamalGuyo <jamalguyo08@gmail.com>
2022-07-02 00:47:54 +0300

    8 base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
