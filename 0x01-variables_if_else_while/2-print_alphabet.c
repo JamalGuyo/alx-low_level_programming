23d3c8d3d2012222f610c38ff7caae8e9c88220f
JamalGuyo <jamalguyo08@gmail.com>
2022-07-01 22:05:41 +0300

    2 print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 is success
+ */
+int main(void)
+{
+	char a = 'a';
+	char z = 'z';
+	while(a <= z)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+}
