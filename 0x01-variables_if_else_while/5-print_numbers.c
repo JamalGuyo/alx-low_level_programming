5d36c107ef84796a2ff4314719edbe79d2aaa478
JamalGuyo <jamalguyo08@gmail.com>
2022-07-01 23:34:20 +0300

    5 base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: o is success
+ */
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
