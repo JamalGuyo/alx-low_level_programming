bcd98320508b35f72765a01a292760f1b3ff48b3
JamalGuyo <jamalguyo08@gmail.com>
2022-07-06 00:03:13 +0300

    11 print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,14 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
-			printf("%d, ", n);
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+			else
+			{
+				printf("%d", n);
+			}
 			n--;
 		}
 	}
@@ -20,7 +27,14 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			printf("%d, ", n);
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+			else
+			{
+				printf("%d", n);
+			}
 			n++;
 		}
 	}
