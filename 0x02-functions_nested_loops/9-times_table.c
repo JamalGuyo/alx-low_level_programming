25d8604d345d87fe99992b7c66b82b35f5e68ec4
JamalGuyo <jamalguyo08@gmail.com>
2022-07-06 01:51:52 +0300

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,51 +1,44 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * times_table - fn
- * Return: return
+ * times_table - prints the 9 times table
+ *
+ * Description: prints the 9 times table
+ *
+ * Return: void
  */
+
 void times_table(void)
 {
-	int cols, rows;
+	int row, column, product, tens, ones;
 
-	for (rows = 0; rows <= 9; rows++)
+	for (row = 0; row <= 9; row++)
 	{
-		for (cols = 0; cols <= 9; cols++)
+		for (column = 0; column <= 9; column++)
 		{
-			int mult = cols * rows;
+			product = row * column;
+			tens = product / 10;
+			ones = product % 10;
 
-			if (mult <= 9)
+			if (column == 0)
 			{
-				if (cols != 0 && cols != 9)
-				{
-					printf("  %d,", mult);
-				}
-				else if (cols == 9)
-				{
-					printf("  %d", mult);
-				}
-				else
-				{
-					printf("%d,", mult);
-				}
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
 			}
 			else
 			{
-				if (cols != 0 && cols != 9)
-				{
-					printf(" %d,", mult);
-				}
-				else if (cols == 9)
-				{
-					printf(" %d", mult);
-				}
-				else
-				{
-					printf("%d,", mult);
-				}
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
 			}
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
