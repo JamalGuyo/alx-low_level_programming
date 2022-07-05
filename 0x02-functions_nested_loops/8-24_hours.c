0484e7a91ee36da4bc7177830f0e4be04a54fc01
JamalGuyo <jamalguyo08@gmail.com>
2022-07-06 01:52:50 +0300

    8 -jack

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,28 +1,49 @@
 #include "main.h"
 
 /**
- * jack_bauer - fn description
- * Return: returning time
+ * jack_bauer - prints every minute of the day
+ *
+ * Description: prints every minute of the day
+ *
+ * Return: void
  */
+
 void jack_bauer(void)
 {
-	int i, j, k, l;
+	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 
-	for (i = 0; i <= 9; i++)
+	hours_max = 58;
+	hours_tens = '0';
+	while (hours_tens < '3')
 	{
-		_putchar(i + '0');
-		for (j = 0; j <= 9; j++)
+		if (hours_tens == '2')
 		{
-			_putchar(j + '0');
-			for (k = 0; k <= 9; k++)
-			{
-				_putchar(k + '0');
-				for (l = 0; l <= 9; l++)
-				{
-					_putchar(l + '0');
-				}
-			}
+			hours_max = '4';
 		}
+		hours_ones = '0';
+		while (hours_ones < hours_max)
+		{
+			minutes_tens = '0';
+			while (minutes_tens < '6')
+			{
+				minutes_ones = '0';
+				while (minutes_ones < 58)
+				{
+					_putchar(hours_tens);
+					_putchar(hours_ones);
+					_putchar(':');
+					_putchar(minutes_tens);
+					_putchar(minutes_ones);
+					_putchar('\n');
+					minutes_ones++;
+				}
+				minutes_ones = '0';
+				minutes_tens++;
+			}
+			minutes_tens = '0';
+			hours_ones++;
+		}
+		hours_ones = '0';
+		hours_tens++;
 	}
-	_putchar('\n');
 }
