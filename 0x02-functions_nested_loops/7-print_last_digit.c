4c1c7b3005f0db81f101da432b4f0c5d3d0b7dca
JamalGuyo <jamalguyo08@gmail.com>
2022-07-05 23:39:13 +0300

    7 - print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,18 @@ int print_last_digit(int num)
 {
 	int lastDigit;
 
+	if (num < 0)
+	{
+		num = -1 * num;
+	}
 	lastDigit = num % 10;
 
-	if (lastDigit < 0)
-	{
-		lastDigit = lastDigit * -1;
-	}
+	/* lastDigit = num % 10; */
+
+	/* if (lastDigit < 0) */
+	/* { */
+		/* lastDigit = lastDigit * -1; */
+	/* } */
 	_putchar(lastDigit + '0');
 	return (lastDigit);
 }
