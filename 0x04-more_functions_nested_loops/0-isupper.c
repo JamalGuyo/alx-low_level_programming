77386195eba90eee5647be9ea1959a07f4752e9f
JamalGuyo <jamalguyo08@gmail.com>
2022-07-07 13:41:30 +0300

    0 is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,13 @@
 /**
  * _isupper - fn finding upper and lower case
  * @c: parameter, a character
+ * Return: 0 is lower and 1 is upper
  */
 int _isupper(int c)
 {
-    if (c > 'a' && c < 'z')
-    {
-        return (0);
-    }
-    return (1);
+	if (c > 'a' && c < 'z')
+	{
+	return (0);
+	}
+	return (1);
 }
