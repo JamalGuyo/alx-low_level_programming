646381543bacc57babed747632de0766ccaeef3b
JamalGuyo <jamalguyo08@gmail.com>
2022-07-07 20:41:24 +0300

    9 fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: 0 is success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i != 100)
+		{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("%s ", "FizzBuzz");
+		}
+		else if (i % 5 != 0 && i % 3 == 0)
+		{
+			printf("%s ", "Fizz");
+		}
+		else if ((i % 5 == 0) && (i % 3 != 0))
+		{
+			printf("%s ", "Buzz");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
