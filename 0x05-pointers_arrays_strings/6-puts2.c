2ce512a0ff8e1066feb8e29097398be0c189ab7a
JamalGuyo <jamalguyo08@gmail.com>
2022-07-11 23:07:51 +0300

    tsk

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,21 @@
-#include <string.h>
 #include "main.h"
 
 /**
- * puts2 - fn to print every other character
- * @str: string parameter
+ * puts2 - print alternating chars of string
+ * @str: string
  * Return: void
  */
+
 void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	i = 0;
+	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
