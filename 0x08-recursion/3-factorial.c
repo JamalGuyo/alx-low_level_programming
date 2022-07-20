2b10979d59c02fc1800934d3f0d87a9d2fc571c1
JamalGuyo <jamalguyo08@gmail.com>
2022-07-20 22:23:57 +0300

    3 factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+/**
+ * factorial - fn to print factorial
+ * @n: integer parameter
+ * Return: result of factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+		return (n * factorial(n - 1));
+}
