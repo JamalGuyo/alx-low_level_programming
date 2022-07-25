debe40a9ade4dc6d5f11182ecc0c919e75dbbf1c
JamalGuyo <jamalguyo08@gmail.com>
2022-07-25 15:22:30 +0300

    1 str duplicate

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - fn to copy string
+ * @str: string to copy
+ * Return: pointer to copied string
+ */
+char *_strdup(char *str)
+{
+	char *p_dup = malloc(sizeof(char) * (strlen(str) + 1));
+	int i;
+
+	if (str == NULL || p_dup == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		p_dup[i] = str[i];
+	}
+
+	return (p_dup);
+}
