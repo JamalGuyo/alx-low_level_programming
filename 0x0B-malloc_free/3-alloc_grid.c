f6ce366bebec3af22dd36a438d95e714e7e17469
JamalGuyo <jamalguyo08@gmail.com>
2022-07-26 14:17:06 +0300

    3 - alloc grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+
+/**
+ * alloc_grid - fn to create alloc grid
+ * @width: width
+ * @height: height
+ * Return: pointer to pointer
+ */
+int **alloc_grid(int width, int height)
+{
+	int **my_array, i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	/* create pointers to the rows */
+	my_array = malloc(sizeof(int *) * height);
+
+	if (my_array == NULL)
+	{
+		free(my_array);
+		return (NULL);
+	}
+
+	/* loop through the rows of pointers and create width*/
+	for (i = 0; i < height; i++)
+	{
+		my_array[i] = malloc(sizeof(int) * width);
+
+		if (my_array[i] == NULL)
+		{
+			free(my_array[i]);
+			return (NULL);
+		}
+
+		/* assign 0 to all the spaces */
+		for (j = 0; j < width; j++)
+		{
+			my_array[i][j] = 0;
+		}
+		free(my_array[i]);
+	}
+
+	free(my_array);
+
+	return (my_array);
+}
