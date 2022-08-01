444b4e9d4000e5ffa2f6596db4b950b13122b1da
JamalGuyo <jamalguyo08@gmail.com>
2022-08-01 17:49:07 +0300

    0 dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - struct to define a dog
+ * @name: string
+ * @age: float
+ * @owner: string
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
